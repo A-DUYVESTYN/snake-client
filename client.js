0a02f27bb6da1789484311e48a2d6b30a7b80f0d
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-06 19:01:53 +0000

    basic movement

--- client.js
+++ client.js
@@ -10,9 +10,30 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // name and console log msg
   conn.on("connect", () => {
     console.log("Connection established");
+    conn.write("Name: WHA");
+
+    // conn.write("Move: up");
+    
+    setTimeout(() => {conn.write("Move: left")}, 500);
+    setTimeout(() => {conn.write("Move: left")}, 500);
+    setTimeout(() => {conn.write("Move: down")}, 1000);
+
+    setInterval(() => {conn.write("Move: right")}, 100);
+
+    
+
   });
+  // movement
+  // conn.on("connect", () => {
+
+    
+
+  // });
+
+  // log data received from server to the console
   conn.on("data", (data) => {
     console.log("Data received: ", data);
   });
